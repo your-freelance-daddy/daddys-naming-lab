15c9ad9cb8f3b5b1f2aeaad178253edf6feb6c78
your-freelance-daddy <borjanisajlovski@gmail.com>
2026-05-05 11:50:17 +0200

    Add fetch import to name-test.js

--- api/name-test.js
+++ api/name-test.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
